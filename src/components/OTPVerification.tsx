e2aed8328e5eff2dd30dfed4d459f6e959525ebd
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-12-18 14:08:36 +0530

    Update OTPVerification.tsx

--- src/components/OTPVerification.tsx
+++ src/components/OTPVerification.tsx
@@ -114,7 +114,7 @@ const OTPVerification: React.FC<OTPVerificationProps> = ({
         {otp.map((digit, index) => (
           <input
             key={index}
-            ref={(el) => (inputRefs.current[index] = el)}
+            ref={(el) => { inputRefs.current[index] = el }}
             type='text'
             inputMode='numeric'
             maxLength={1}
